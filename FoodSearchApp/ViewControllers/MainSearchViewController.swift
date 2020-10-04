93a02878816bc8f80aa85aec200d02fdedf79ad8
Carlos Vázquez <cvazquezg19@gmail.com>
2020-10-04 12:25:12 -0500

    first commit

---
+++ FoodSearchApp/ViewControllers/MainSearchViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainSearchViewController.swift
+//  FoodSearchApp
+//
+//  Created by Carlos Vazquez on 04/10/20.
+//
+
+import UIKit
+
+class MainSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
